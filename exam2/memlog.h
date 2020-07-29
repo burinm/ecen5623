4294ead49b81c3b237ae012024ad3d54448c52bd
burin <21989631+burinm@users.noreply.github.com>
2020-07-29 16:55:08 -0600

    Exam2 sequencer start

---
+++ exam2/memlog.h
@@ -0,0 +1,85 @@
+/* memlog.h (c) burin 2020
+
+    Quick memory logger for timestamps/events. Use one per thread,
+    then there is no need to worry about concurrency ;)
+    Put logs back together at end.
+
+    TODO - this is a circular(ish) buffer. If all tasks run independantly,
+    then we will be missing events if tasks run at different rates
+*/  
+
+#include <stdint.h>
+#include <string.h> //memset
+#include <time.h>
+//#define MEMLOG_MAX  1024
+#define MEMLOG_MAX  110000
+
+typedef struct _entry {
+    //Running 32bit arm, should be aligned?
+    uint32_t event_id; 
+    struct timespec time;
+} memlog_s;
+
+typedef struct _memlog_g {
+    uint32_t index;
+    memlog_s log[MEMLOG_MAX];
+} memlog_t;
+
+/* memlog events */
+//Program flow events
+#define MEMLOG_E_NONE           0x0
+#define MEMLOG_E_S1_RUN         0x1
+#define MEMLOG_E_W1_RUN         0x2
+#define MEMLOG_E_W2_RUN         0x3
+#define MEMLOG_E_SEQUENCER      0x4
+
+#define MEMLOG_E_S1_DONE       0x11
+#define MEMLOG_E_W1_DONE       0x12
+#define MEMLOG_E_W2_DONE       0x13
+
+#define MEMLOG_E_WCET_START    0x1a
+#define MEMLOG_E_WCET_DONE     0x1b
+
+//Errors
+#define MEMLOG_E_ERROR_SCAN    0x20
+
+//Data logging events
+#define MEMLOG_E_ADATA_24       0xf0
+#define MEMLOG_E_BDATA_24       0xf1
+#define MEMLOG_E_CDATA_24       0xf2
+#define MEMLOG_E_FIB_TEST      0xff
+
+//Field manipulation
+#define MEMLOG_ID_MASK             0xff
+#define MEMLOG_ID(x)               (x & MEMLOG_ID_MASK)
+
+//Field data manipulation
+#define MEMLOG_DATA24(x)           (x >> 8)
+#define MEMLOG_ENCODE24(event, data)    (event + (data << 8))
+
+
+#if 0
+#define MEMLOG_LOG(l, event, t)   (l)->log[(l)->index].event_id = event; \
+                               clock_gettime(CLOCK_REALTIME, &(l)->log[(l)->index].time); \
+                               (l)->index++; \
+                               if ((l)->index == MEMLOG_MAX) { \
+                                    (l)->index = 0; \
+                               } 
+#endif
+void MEMLOG_LOG(memlog_t* l, uint32_t event);
+void MEMLOG_LOG24(memlog_t* l, uint32_t event, uint32_t data);
+
+
+memlog_t* memlog_init();
+void memlog_free(memlog_t* m);
+
+void memlog_dump(char* f, memlog_t* l);
+void memlog_gnuplot_dump(memlog_t* l);
+char* memlog_event_desc(uint32_t e);
+
+/*private*/
+#define NS_PER_SEC  1000000000UL
+#define US_PER_SEC  1000000UL
+                                  
+uint64_t time_in_us(struct timespec t);
+
---
+++ exam2/sequencer.c
@@ -0,0 +1,254 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <string.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <limits.h>
+#include <errno.h>
+
+#include "realtime.h"
+#include "timetools.h"
+#include "memlog.h"
+
+void ctrl_c(int s);
+void sequencer(int v);
+
+sem_t sem_S1;
+pthread_t thread_S1;
+void* S1(void* v);
+memlog_t* S1_LOG;
+
+sem_t sem_W1;
+pthread_t thread_W1;
+void* W1(void* v);
+memlog_t* W1_LOG;
+
+sem_t sem_W2;
+pthread_t thread_W2;
+void* W1(void* v);
+memlog_t* W2_LOG;
+
+pthread_attr_t rt_sched_attr;  // For realtime H/M/L threads
+
+void* S1(void* v);
+void* W1(void* v);
+void* W2(void* v);
+
+
+
+#define NUM_ITERS   30000 // 5minutes / 10ms = 30000
+
+//#define PERIOD_NS   10000000
+#define PERIOD_NS   10000000 //10ms
+
+int running = 1;
+
+int main() {
+
+//Ctrl C
+struct sigaction s0;
+s0.sa_handler = ctrl_c;
+sigaction(SIGINT, &s0, NULL);
+
+//Catch SIGALRM and run seqencer
+struct sigaction s1;
+s1.sa_handler = sequencer;
+sigaction(SIGALRM, &s1, NULL);
+
+//Logging on
+S1_LOG = memlog_init();
+W1_LOG = memlog_init();
+W2_LOG = memlog_init();
+
+//Setup semaphores
+if (sem_init(&sem_S1, 0, 0) == -1) {
+    perror("Couldn't init semaphore sem_framegrab");
+    exit(-1);
+}
+
+if (sem_init(&sem_W1, 0, 0) == -1) {
+    perror("Couldn't init semaphore sem_framegrab");
+    exit(-1);
+}
+
+if (sem_init(&sem_W2, 0, 0) == -1) {
+    perror("Couldn't init semaphore sem_framegrab");
+    exit(-1);
+}
+
+if (set_main_realtime() == -1) {
+    exit(-1);
+}
+
+
+printf("Creating S1\n");
+schedule_realtime(&rt_sched_attr);
+schedule_priority(&rt_sched_attr, MID_PRI);
+
+if (pthread_create(&thread_S1, &rt_sched_attr, S1, NULL) == -1) {
+    perror("Couldn't create frame grabber thread");
+    exit(-1);
+}
+
+printf("Creating W1\n");
+schedule_realtime(&rt_sched_attr);
+schedule_priority(&rt_sched_attr, HIGH_PRI);
+
+if (pthread_create(&thread_W1, &rt_sched_attr, W1, NULL) == -1) {
+    perror("Couldn't create frame grabber thread");
+    exit(-1);
+}
+
+printf("Creating W2\n");
+schedule_realtime(&rt_sched_attr);
+schedule_priority(&rt_sched_attr, LOW_PRI);
+
+if (pthread_create(&thread_W1, &rt_sched_attr, W2, NULL) == -1) {
+    perror("Couldn't create frame grabber thread");
+    exit(-1);
+}
+
+//Interval timer for sequencer loop
+timer_t timer1; // note not defined with a struct
+
+
+//Install timer
+if (timer_create(CLOCK_MONOTONIC, NULL, &timer1) == -1 ) {
+    perror("Couldn't create timer");
+    exit(-1);
+}
+
+struct itimerspec it;
+it.it_interval.tv_sec = 0;
+it.it_interval.tv_nsec = PERIOD_NS; //10ms
+it.it_value.tv_sec = 1; //delay 1 second to start
+it.it_value.tv_nsec = 0;
+
+if (timer_settime(timer1, 0, &it, NULL) == -1 ) {
+    perror("couldn't set timer");
+    exit(-1);
+}
+
+
+printf("Ready.\n");
+pthread_join(thread_S1, NULL);
+pthread_join(thread_W1, NULL);
+pthread_join(thread_W2, NULL);
+
+memlog_dump("S1.log", S1_LOG);
+memlog_dump("W1.log", W1_LOG);
+memlog_dump("W2.log", W2_LOG);
+
+
+}
+
+int seq_count = 0;
+void sequencer(int v) {
+
+    int sequence = 0;
+
+    //printf("Start sequencer\n");
+
+    while(running) {
+        if (sequence % 5 == 0) { // 5 * 10 = 50ms, 20Hz
+            sem_post(&sem_S1);
+        }
+
+        if (sequence % 10 == 0) { // 10 * 10 = 100ms, 10Hz
+            sem_post(&sem_W1);
+        }
+
+        if (sequence % 100 == 0) { // 100 * 10 = 1000ms, 1Hz
+            sem_post(&sem_W2);
+        }
+
+        sequence++;
+
+        if (sequence == 500) {
+            sequence = 0;
+            //printf("tick\n");
+        }
+    }
+
+}
+
+void* S1(void* v) {
+    int ret = -1; 
+    int count = 0;
+
+    printf("S1 started\n");
+    while(running) {
+
+        printf("S1 waiting\n");
+        ret = sem_wait(&sem_S1);
+
+        MEMLOG_LOG(S1_LOG, MEMLOG_E_S1_RUN);
+        count++;
+        printf("send %d\n", count);
+        //do work
+        MEMLOG_LOG(S1_LOG, MEMLOG_E_S1_DONE);
+
+        if (count > 200) {
+            running = 0;
+        }
+
+
+        if (ret == -1) {
+            perror("sem_wait sem_S1 failed");
+            if (errno == EINTR) {
+                return ((void*)-2); 
+            }
+        }
+    }
+return ((void*)0); 
+}
+
+void* W1(void* v) {
+    int ret = -1; 
+
+    printf("W1 started\n");
+    while(running) {
+
+        ret = sem_wait(&sem_W1);
+
+        MEMLOG_LOG(W1_LOG, MEMLOG_E_W1_RUN);
+        //do work
+        MEMLOG_LOG(W1_LOG, MEMLOG_E_W1_DONE);
+
+
+        if (ret == -1) {
+            perror("sem_wait sem_W1 failed");
+            if (errno == EINTR) {
+                return ((void*)-2); 
+            }
+        }
+    }
+return ((void*)0); 
+}
+
+void* W2(void* v) {
+    int ret = -1; 
+
+    printf("W2 started\n");
+    while(running) {
+
+        ret = sem_wait(&sem_W2);
+        MEMLOG_LOG(W2_LOG, MEMLOG_E_W2_RUN);
+        //do work
+        MEMLOG_LOG(W2_LOG, MEMLOG_E_W2_DONE);
+
+        if (ret == -1) {
+            perror("sem_wait sem_W2 failed");
+            if (errno == EINTR) {
+                return ((void*)-2); 
+            }
+        }
+    }
+return ((void*)0); 
+}
+
+void ctrl_c(int s) {
+    running = 0;
+}
